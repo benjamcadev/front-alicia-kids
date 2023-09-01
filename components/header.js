03c5f50476dbec3f75b0169e753603a84259fca6
Benjamin Cortes <benjamca@hotmail.com>
2023-09-01 16:53:08 -0400

    Diseñando el header + responsivo

--- components/header.js
+++ components/header.js
@@ -4,21 +4,34 @@ import styles from '../styles/header.module.css'
 import logo from '../public/img/alicia-kids-logo.png'
 
 export default function Header() {
-    return (
-     <header className={styles.header}>
-        <div className="contenedor">
-         <Image src={logo.src}  width={160} height={140} alt='imagen logotipo'/>
+   return (
+      <header className={styles.header}>
+         <div className="contenedorHeader">
+            <div className={styles.logoHeader}>
+               <Image src={logo.src} width={160} height={140} alt='imagen logotipo' />
+            </div>
 
-         <nav>
-            <Link  href="/">
-               Inicio
-            </Link>
+            <div className={styles.navHeader}>
+               <nav className={styles.navegacion}>
+                  <Link className={styles.enlace} href="/">
+                     Inicio
+                  </Link>
 
-            <Link href="/contacto">
-               Contacto
-            </Link>
-         </nav>
-        </div>
-     </header>
-    )
-  }
+                  <Link href="/contacto">
+                     Contacto
+                  </Link>
+
+                  <Link href="/juegos">
+                     Juegos
+                  </Link>
+
+                  <Link href="/reserva">
+                     Reserva tu hora
+                  </Link>
+               </nav>
+            </div>
+
+         </div>
+      </header>
+   )
+}
