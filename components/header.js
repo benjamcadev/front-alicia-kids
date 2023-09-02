e0e7fbc6755d0a1698622f6a275fa1a088419a97
Benjamin Cortes <benjamca@hotmail.com>
2023-09-02 13:58:56 -0400

    Finalizando el header

--- components/header.js
+++ components/header.js
@@ -1,33 +1,55 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 import logo from '../public/img/alicia-kids-logo.png'
+import { GrInstagram } from "react-icons/gr";
+import { ImWhatsapp } from 'react-icons/im'
 
 export default function Header() {
+
+   const router = useRouter()
+
    return (
       <header className={styles.header}>
          <div className="contenedorHeader">
             <div className={styles.logoHeader}>
-               <Image src={logo.src} width={160} height={140} alt='imagen logotipo' />
+               <Link href={'/'}>
+                <Image src={logo.src} width={160} height={140} alt='imagen logotipo' />
+               </Link>
+              
             </div>
 
             <div className={styles.navHeader}>
                <nav className={styles.navegacion}>
-                  <Link className={styles.enlace} href="/">
+                  <Link className={ router.pathname === '/' ? styles.enlace +' '+ styles.active : styles.enlace} href="/">
                      Inicio
                   </Link>
 
-                  <Link href="/contacto">
+                  <Link className={router.pathname === '/contacto' ? styles.enlace +' '+ styles.active : styles.enlace} href="/contacto">
                      Contacto
                   </Link>
 
-                  <Link href="/juegos">
-                     Juegos
+                  <Link className={router.pathname === '/juegos' ? styles.enlace +' '+ styles.active : styles.enlace} href="/juegos">
+                     Juegos 
                   </Link>
 
-                  <Link href="/reserva">
+                  <Link className={router.pathname === '/reserva' ? styles.enlace +' '+ styles.active : styles.enlace} href="/reserva">
                      Reserva tu hora
                   </Link>
+
+                  <div className={styles.enlace +' '+ styles.rrss}>
+
+                  <Link className={styles.rrss}  href="/ig">
+                   <GrInstagram />
+                  </Link>
+
+                  <Link className={styles.rrss}  href="/wts">
+                   <ImWhatsapp />
+                  </Link>
+                  </div>
+                 
+
                </nav>
             </div>
 
