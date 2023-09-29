1ed367e5dd2b71166e89d4f93dd0b928afa898f5
Benjamin Cortes <benjamca@hotmail.com>
2023-09-29 15:48:06 -0300

    SSR en el index para IG

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ feed }) {
   )
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink,username&access_token=${process.env.INSTAGRAM_TOKEN}`
   const data = await fetch(url)
   const feed = await data.json()
