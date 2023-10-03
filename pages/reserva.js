536097626383c0eecfd3d3d319a181c679cc20bb
Benjamin Cortes <benjamca@hotmail.com>
2023-10-03 14:04:26 -0300

    Agregando date time picker

--- pages/reserva.js
+++ pages/reserva.js
@@ -4,12 +4,21 @@ import FormWizard from "react-form-wizard-component";
 import swal from 'sweetalert'
 import 'react-form-wizard-component/dist/style.css';
 
+// date time picker
+import dayjs from 'dayjs'
+import 'dayjs/locale/de';
+import { esES } from '@mui/x-date-pickers/locales';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+
+
 import styles from '../styles/reserva.module.css'
 
 export default function Reserva() {
 
-    const [user, setUser] = useState({ userEmail: "", userName: "" });
-   
+    const [user, setUser] = useState({ userEmail: "bmcortes@agroplastic.cl", userName: "Benjamin" });
+
 
 
     const handleComplete = () => {
@@ -29,7 +38,7 @@ export default function Reserva() {
     const checkValidateTab = () => {
 
         console.log(user);
-        
+
 
         if (user.userEmail === "" || user.userName === "") {
             return false;
@@ -50,9 +59,25 @@ export default function Reserva() {
     const errorMessages = (e) => {
         // you can add alert or console.log or any thing you want
 
-        swal("Good job!", "You clicked the button!", "warning");
+        swal("Ups!", "Algo falta por completar o esta mal escrito!", "warning");
         setUser({ userEmail: "", userName: "" })
     };
+
+    const nextTemplate = (handleNext) => {
+        return (
+            <button className={styles.contenidoButtons + ' ' + styles.buttonRight} onClick={handleNext}>
+                Siguiente
+            </button>
+        )
+    }
+
+    const backTemplate = (handlePrevious) => {
+        return (
+            <button className={styles.contenidoButtons + ' ' + styles.buttonLeft} onClick={handlePrevious}>
+                Atras
+            </button>
+        )
+    }
 
 
     return (
@@ -65,38 +90,50 @@ export default function Reserva() {
                 <h2 className="heading">Reserva tu hora</h2>
                 <div className={styles.contenido}>
                     <FormWizard
+                        stepSize="sm"
                         onComplete={handleComplete}
                         onTabChange={tabChanged}
+                        nextButtonTemplate={nextTemplate}
+                        backButtonTemplate={backTemplate}
+                        color="#c23535"
                     >
-                        <FormWizard.TabContent title="Personal details" >
+                        <FormWizard.TabContent title="Ingresa Datos" >
 
-                            <h3>Hola ! </h3>
-                            <p>Ingresa tus datos</p>
+                            <h3>Hola ! 👋 Ingresa tus datos </h3>
+
                             <input
                                 type="text"
                                 value={user.userName}
                                 name="name"
                                 placeholder="Ingresa tu nombre"
-                                onChange={(e) => setUser({...user, userName: e.target.value })}
+                                onChange={(e) => setUser({ ...user, userName: e.target.value })}
                             />
                             <input
                                 type="email"
                                 value={user.userEmail}
                                 name="email"
                                 placeholder="Ingresa tu correo"
-                                onChange={(e) => setUser({...user, userEmail: e.target.value })}
+                                onChange={(e) => setUser({ ...user, userEmail: e.target.value })}
                             />
 
                         </FormWizard.TabContent>
                         <FormWizard.TabContent
-                            title="Additional Info"
+                            title="Selecciona Fecha"
                             isValid={checkValidateTab()}
                             validationError={errorMessages}
                         >
-                            <h3>Second Tab</h3>
-                            <p>Some content for the second tab</p>
+                            <h3>Hola {user.userName}, Ahora selecciona una fecha</h3>
+                            <LocalizationProvider 
+                            dateAdapter={AdapterDayjs}
+                            localeText={esES.components.MuiLocalizationProvider.defaultProps.localeText} >
+                                <MobileDateTimePicker
+                                    defaultValue={dayjs()}
+                                    format="YYYY-MM-DD HH:mm:ss"
+                                    slotProps={{ textField: { size: 'normal' } }}
+                                />
+                            </LocalizationProvider>
                         </FormWizard.TabContent>
-                        <FormWizard.TabContent title="Last step">
+                        <FormWizard.TabContent title="Direccion y Contacto">
                             <h3>Last Tab</h3>
                             <p>Some content for the last tab</p>
                         </FormWizard.TabContent>
