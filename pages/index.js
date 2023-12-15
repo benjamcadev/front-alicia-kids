413b2c61ed343161d56312f6ad2a99a45154ea76
Benjamin Cortes <benjamca@hotmail.com>
2023-12-15 18:45:28 -0300

    Se agrega page de login

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,8 @@ export default function Home({ feed }) {
           <Slider />
           <Presentacion />
           <Clientes />
-          <FeedInstagram feedinstagram={feed} />
+          {(feed.error) ? <h3>Problemas para cargar Instagram , contacte soporte</h3> : <FeedInstagram feedinstagram={feed} /> }
+          
 
         </main>
 
@@ -34,6 +35,8 @@ export const getServerSideProps = async () => {
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink,username&access_token=${process.env.INSTAGRAM_TOKEN}`
   const data = await fetch(url)
   const feed = await data.json()
+
+
 
   return {
     props: {
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <main  className="contenedorBody">
+            
+        </main>
+    </div>
+  )
+}
